f603fa1cdda992b60baf8c0f4d19474c9d5935d9
qubrat <kuba.ratajczyk.biz@gmail.com>
2023-03-10 21:14:13 +0100

    Riddles

--- js/riddles/index.js
+++ js/riddles/index.js
@@ -1,16 +1,28 @@
 // 1. odwróć liczbę
 // np dla 12345, funkcja powinna zwrócić 54321
-function reverseNumber() {
+function reverseNumber(num) {
   // uzupełnij tutaj
+  let digit,
+    result = 0;
+  while (num) {
+    digit = num % 10;
+    result = result * 10 + digit;
+    num = (num / 10) | 0;
+  }
+  return result;
 }
 
 console.log("1.", reverseNumber(12345));
 
-// 2. doodaj do siebie wszystkie wartości z tablicy, które są parzyste
+// 2. dodaj do siebie wszystkie wartości z tablicy, które są parzyste
 // dla tablicy tab powinniśmy otrzymać 2 + 4 + 6 + 8 = 20
 const tab = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function addEven(array) {
   // uzupełnij tutaj
+  return array.reduce(
+    (acc, current) => acc + (current % 2 === 0 ? current : 0),
+    0
+  );
 }
 
 console.log("2.", addEven(tab));
